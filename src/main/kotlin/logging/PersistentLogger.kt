e8bde8e68f9177f5491737f72b773b5e849007a4
Firzen7 <firzen.developer@gmail.com>
2026-03-23 18:07:31 +0100

    Connected persistent logger with server logic

--- src/main/kotlin/logging/PersistentLogger.kt
+++ src/main/kotlin/logging/PersistentLogger.kt
@@ -1,13 +1,32 @@
 package net.firzen.web.logging
 
 import net.firzen.web.LOG_DIRECTORY
+import net.firzen.web.UNKNOWN_USER
 import net.firzen.web.dateTimeString
 import org.joda.time.DateTime
 import java.io.File
 
 object PersistentLogger {
-    fun logAction(user: String, action: String) {
+    fun logAction(logLevel: LogLevel, rawUser: String?, action: String) {
         val currentTime = DateTime().dateTimeString()
-        File("${LOG_DIRECTORY}/$user.log").appendText("$currentTime: $action")
+        val user = rawUser ?: UNKNOWN_USER
+        val logPath = "${LOG_DIRECTORY}/$user.log"
+
+        try {
+            val logfile = File(logPath)
+
+            if (!logfile.exists()) {
+                logfile.createNewFile()
+            }
+
+            if (logfile.canWrite()) {
+                logfile.appendText("$logLevel $currentTime: $action\n")
+            } else {
+                Logger.e("Could not write into logfile! Path: $logPath")
+            }
+        }
+        catch (_: Exception) {
+            Logger.e("Could not write into logfile! Path: $logPath")
+        }
     }
 }
