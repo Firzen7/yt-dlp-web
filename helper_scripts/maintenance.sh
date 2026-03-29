6dcc94c5296f99a741029976f8a0eb33425e7d10
Firzen7 <firzen.developer@gmail.com>
2026-03-29 17:56:08 +0200

    Maintenance script corrected

--- helper_scripts/maintenance.sh
+++ helper_scripts/maintenance.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# This script removed old files from yt-dlp-web downloads directory,
+# This script removes old files from yt-dlp-web downloads directory,
 # and it also updates yt-dlp utility.
 # It is to be configured to run regularly using cron.
 
 TARGET="/tmp/yt-dlp-web"
 # TARGET="/home/firzen/tmp"
 
-find "$TARGET" -mindepth 1 -maxdepth 1 -type d -mtime +3 -print
+find "$TARGET" -mindepth 1 -maxdepth 1 -type d -mtime +3 -exec rm -rf {} +
 
 yt-dlp -U
 
